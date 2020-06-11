b4ddb88dd3e7e85cc913361d8880932019dffa23
Shamil Samusenko <Shamilllaa@mail.ru>
2020-06-11 14:52:29 +0700

    lesson 5 complicated

--- script/script.js
+++ script/script.js
@@ -2,27 +2,57 @@
 
 // Функция проверки на число
 let isNumber = function (n) {
-    return !isNaN(parseFloat(n)) && isFinite(n);
+	return !isNaN(parseFloat(n)) && isFinite(n);
 };
 
-// 
-const guessNumber = function (number) {
-    let userNumber = prompt('Угадай число от 1 до 100');
-
-    if (!isNumber(userNumber) && userNumber !== null) {
-        alert('Введи число!');
-        guessNumber(32);
-    } else if (!!(+userNumber)) {
-        if (userNumber > number) {
-            alert('Загаданное число меньше!');
-            guessNumber(32);
-        } else if (userNumber < number) {
-            alert('Загаданное число больше!');
-            guessNumber(32);
-        } else {
-            alert(`Поздравляем! Загаданное число: ${number}`);
-        }
-    }
+// Функция рандомного числа
+let getRandomNumber = function (rangeMin, rangeMax) {
+	let number = Math.ceil(Math.random() * (rangeMax - rangeMin) + rangeMin);
+	return number;
 };
 
-guessNumber(32);
+const guessNumber = function (number, attempt) {
+	let userNumber = prompt('Угадай число от 1 до 100');
+	if (!isNumber(userNumber) && userNumber !== null) {
+		alert('Введи число!');
+		guessNumber(number, attempt);
+	} else if (!!+userNumber) {
+		if (attempt > 1 && attempt <= 10) {
+			if (userNumber > number) {
+				attempt--;
+				alert(`Загаданное число меньше, осталось попыток ${attempt}`);
+				console.log('Загаданное число: ', number);
+				guessNumber(number, attempt);
+			} else if (userNumber < number) {
+				attempt--;
+				alert(`Загаданное число больше, осталось попыток ${attempt}`);
+				console.log('Загаданное число: ', number);
+				guessNumber(number, attempt);
+			} else {
+				let newStep = confirm(
+					`Поздравляем! Загаданное число: ${number}. Хотите сыграть еще?`
+				);
+				if (newStep) {
+					attempt = 10;
+					number = getRandomNumber(1, 100);
+					console.log('Загаданное число: ', number);
+					guessNumber(number, attempt);
+				} else {
+					alert('Игра закончена! До новых встреч!');
+				}
+			}
+		} else {
+			let theEnd = confirm('Попытки закончились, хотите сыграть еще?');
+			if (theEnd) {
+				attempt = 10;
+				number = getRandomNumber(1, 100);
+				console.log('Загаданное число: ', number);
+				guessNumber(number, attempt);
+			}
+		}
+	} else {
+		alert('Игра закончена! До новых встреч!');
+	}
+};
+
+guessNumber(getRandomNumber(1, 100), 10);
